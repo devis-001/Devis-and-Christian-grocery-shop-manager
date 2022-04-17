d5373877fc2b8d5a88845d1f60990a605c45dccc
devis-001 <devkcheruiyot@yahoo.com>
2022-04-17 10:17:15 -0600

    first commit

---
+++ src/com/grocery/store/USERS/Buyer.java
@@ -0,0 +1,20 @@
+package com.grocery.store.USERS;
+
+public class Buyer extends User{
+    private double wallet;
+
+    public Buyer(String firstName, String lastName, String email, String phone, String password) {
+        super(firstName, lastName, email, phone, password);
+        this.wallet=0;
+    }
+
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
+
+}
